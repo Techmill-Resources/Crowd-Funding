ea0d409d8e2e738add9ba3492cc1c66ff4ff3e98
Golden Ogbeka <ogbekagolden@gmail.com>
2021-03-02 09:49:46 +0100

    Initial commit

---
+++ UserAuthentication/server.js
@@ -0,0 +1,11 @@
+//setup server
+
+//Setup Database connection using test database
+
+//setup jwt
+
+//setup middleware
+const verifyUser= ((req,res)=>{
+    // Connect to DB
+    //Check for user
+})
